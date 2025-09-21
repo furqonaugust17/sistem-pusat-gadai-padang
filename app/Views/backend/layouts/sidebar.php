8df80927bb0948dfa4e6b3b0e74b54894ba06553
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-09-21 13:31:32 +0700

    fix: fix menu url

--- app/Views/backend/layouts/sidebar.php
+++ app/Views/backend/layouts/sidebar.php
@@ -9,7 +9,7 @@
             <p class="email">marquezzzz@mail.com</p>
         </div>
         <ul class="metismenu" id="menu">
-            <li><a href="<?= route_to('karyawan'); ?>" class="ai-icon" aria-expanded="false">
+            <li><a href="<?= route_to('KaryawanController::index'); ?>" class="ai-icon" aria-expanded="false">
                     <i class="fas fa-users fw-bold"></i>
                     <span class="nav-text">Karyawan</span>
                 </a>
